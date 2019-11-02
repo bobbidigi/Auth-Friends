b6ef6afd585f314b3cd1e9c678b6bf7f85bf2a2c
bobbidigi <bobbidigi34@gmail.com>
2019-11-02 12:43:45 -0400

    add friends

--- friends/src/components/App.js
+++ friends/src/components/App.js
@@ -6,6 +6,7 @@ import ProtectedRoute from './ProtectedRoute'
 import Signin from './Signin'
 import Account from './Account'
 import Logout from './Logout'
+import Add from './Add'
 
 function App() {
 
@@ -18,10 +19,13 @@ function App() {
 				{!signedIn && <Link to="/signin">Sign In</Link>}
 				{signedIn && <Link to="/account">My Account</Link>}
 				{signedIn && <Link to="/logout">Log Out</Link>}
+				{signedIn && <Link to="/add">Add</Link>}
+
 			</nav>
 			<Route exact path='/signin' component={Signin}/>
 			<ProtectedRoute exact path='/account' component={Account}/>
 			<ProtectedRoute exact path='/logout' component={Logout}/>
+			<ProtectedRoute exact path='/add' component={Add}/>
 		</div>
 	)
 }
